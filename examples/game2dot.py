9209c4bc4e5dae23f0b786cf1b441c36ac3f3e3e
Andy Kitchen <kitchen.andy+git@gmail.com>
2018-03-01 12:02:36 +1100

    cleaned up game2dot

--- examples/game2dot.py
+++ examples/game2dot.py
@@ -6,10 +6,14 @@ import oz
 # g = oz.RockPaperScissors()
 g = oz.KuhnPoker()
 
-def print_indent(n, s):
-    print("\t"*n + s)
+infoset_style = 'cluster'
+# infoset_style = 'edge'
 
+infoset_nodes = {}
 n_nodes = 0
+
+def print_indent(n, s):
+    print("\t"*n + str(s))
 
 def gen_node():
     global n_nodes
@@ -17,16 +21,15 @@ def gen_node():
     n_nodes = n_nodes + 1
     return name
 
-node_infoset = {}
-
-def dot_game(g, node=gen_node(), h=[]):
-    if (not g.is_terminal()) and (g.player != g.Player.Chance):
+def print_dot(g, node=gen_node(), h=[]):
+    if not g.is_terminal() and g.player != g.Player.Chance:
         infoset = g.infoset(g.player)
-        ls = node_infoset.setdefault(infoset, [])
+        ls = infoset_nodes.setdefault(infoset, [])
         ls.append(node)
 
     if g.is_terminal():
-        l = '{} [label="{}" shape=plaintext]'.format(node, g.reward())
+        r = g.reward()
+        l = '{} [label="{}" shape=plaintext]'.format(node, r)
         print_indent(1, l)
     else:
         player = g.player
@@ -45,28 +48,40 @@ def dot_game(g, node=gen_node(), h=[]):
             g_a = copy(g)
             g_a.act(a)
             label = str(a).split(".")[-1]
-            props = 'headport=_ tailport=c label="{}"'.format(label)
+            props = 'headport=_ tailport=c label="{}" len=1.5'.format(label)
             if not g_a.is_terminal():
-                props += ' weight=2'
+                props += ' weight=4'
             else:
-                props += ' weight=1'
+                props += ' weight=2'
             l = '{} -> {} [{}]'.format(node, node_a, props)
             print_indent(1, l)
-            dot_game(g_a, node_a, h + [a])
+            print_dot(g_a, node_a, h + [a])
 
-print('digraph {')
+def print_infoset_clusters(infoset_nodes):
+    for infoset, nodes in infoset_nodes.items():
+        print_indent(1, 'subgraph "cluster_infoset_{}" {{'.format(infoset))
+        print_indent(2, 'label="{}"'.format(infoset))
+        print_indent(2, 'style=dashed')
+        for node in nodes:
+            print_indent(2, node)
+        print_indent(1, '}')
+
+def print_infoset_edges(infoset_nodes):
+    for infoset, nodes in infoset_nodes.items():
+        for a, b in zip(nodes, nodes[1:]):
+            props = 'dir=none style=dashed label="{}" weight=1 len=1'.format(infoset)
+            print_indent(1, '{} -> {} [{}]'.format(a, b, props))
+
+print_indent(0, 'digraph {')
 
 print_indent(1, 'graph [nodesep=0.2 ranksep=1.5 splines=true]')
-
 print_indent(1, 'node [height=.5 width=.5]')
-dot_game(g)
 
-for infoset, nodes in node_infoset.items():
-    print_indent(1, 'subgraph "cluster_infoset_{}" {{'.format(infoset))
-    print_indent(2, 'label="{}"'.format(infoset))
-    print_indent(2, 'style=dashed')
-    for node in nodes:
-        print_indent(2, node)
-    print_indent(1, '}')
+print_dot(g)
 
-print('}')
+if infoset_style == 'cluster':
+    print_infoset_clusters(infoset_nodes)
+elif infoset_style == 'edge':
+    print_infoset_edges(infoset_nodes)
+
+print_indent(0, '}')
