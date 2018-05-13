2cfdba59aecdcabd479dfef53dbf6c32f71e9032
Andy Kitchen <kitchen.andy+git@gmail.com>
2018-05-13 23:31:58 +1000

    added more experiment scripts

--- experiments/train/goof6-a-10k.sh
+++ experiments/train/goof6-a-10k.sh
@@ -1,4 +1,4 @@
-EXPERIMENT=leduk-a-100k
+EXPERIMENT=goof6-a-10k
 
 SRC_DIR=/mnt/src/oz
 PYTHON=/mnt/miniconda3/bin/python
@@ -8,19 +8,20 @@ LOG_PATH=/mnt/exp/${EXPERIMENT}.txt
 OMP_NUM_THREADS=1 \
 PYTHONPATH=$SRC_DIR \
 nohup $PYTHON $SRC_DIR/script/train.py \
-	--game=leduk \
-	--dist --workers=8 \
+	--game=goofspiel \
+	--goofcards=6 \
+	--dist --workers=32 \
 	--checkpoint_dir=$CHECKPOINT_DIR \
 	--checkpoint_interval=1 \
 	--train_iter=2500 \
-	--train_game_ply=16 \
-	--train_batch_size=$[4*16] \
+	--train_game_ply=6 \
+	--train_batch_size=$[6*32] \
 	--train_steps=128 \
-	--nn_arch=mlp \
-	--hidden_size=128 \
+	--nn_arch=deep \
+	--hidden_sizes="128:64" \
 	--search_batch_size=20 \
-	--reservoir_size=$[2**16] \
+	--reservoir_size=$[1000*6*32] \
 	--reservoir_beta_ratio=2.0 \
-	--simulation_iter=100000 \
-	--print_ex --progress \
+	--simulation_iter=10000 \
+	--progress \
 	>>$LOG_PATH 2>&1 <&- &
