85d032e932f694d92a2554f7b71baebdfb10e41d
root <michela.benedetti89@gmail.com>
2018-11-18 23:47:27 +0000

    Cleaned game tic_tac_toes

--- oz/csrc/games/tic_tac_toes.cpp
+++ oz/csrc/games/tic_tac_toes.cpp
@@ -2,7 +2,6 @@
 #include "hash.h"
 
 #include <cassert>
-#include <memory>
 #include <vector>
 #include <sstream>
 #include <string>
@@ -35,43 +34,31 @@ namespace oz {
               return 7;
           case tic_tac_toes_t::action_t::fill_9 :
               return 8;
-          default: return -1;
-          
-          }
+          case tic_tac_toes_t::action_t::NextRound :
+              return -1;
+          default: assert(false); 
               
+          }
   }
     
-    
-
+  
   void tic_tac_toes_t::act_(action_t a) {
     
-    if (action_number > MAX_VALUE_ACTION ) {
+    if (action_number > MAX_SQUARES ) {
       throw std::invalid_argument("maximum number of moves reached");
     }
-    if (player_idx(player_) == 0){
-      if (action_number == MAX_VALUE_ACTION){
-        is_terminal_flag = is_winning_move_vector(pieces_P1_);
-        if (is_terminal_flag == 0)
-          is_terminal_flag = 2;
-      }
-    }
-    else{
-      if (action_number == MAX_VALUE_ACTION){
-        is_terminal_flag = is_winning_move_vector(pieces_P2_);
-        if (is_terminal_flag == 0)
-          is_terminal_flag = 2;
-      }
-    }
-    
+      
     history_.push_back(a);
-    //cout << infoset().str() << endl;
+    
+    if (show_move == 1)
+        cout << infoset().str() << endl;
       
       
     // case current player is Player 1
     if (player_idx(player_) == 0){
         
         // calculate opponent past actions 
-        int past_actions[MAX_SQUARES] = {0,0,0,0,0,0,0,0};
+        int past_actions[MAX_SQUARES] = {0,0,0,0,0,0,0,0, 0};
         for (const auto& a : pieces_P2_){
                int idx = actions_to_idx(a);
                past_actions[idx] = 1;
@@ -88,30 +75,31 @@ namespace oz {
             pieces_P1_.push_back(action_t::NextRound);
             
             history_.push_back(action_t::NextRound);
-            
-            
-            is_terminal_flag = is_winning_move_vector(pieces_P1_);
     
-                                             
+            is_terminal_flag = is_winning_move_vector(pieces_P1_);
+                                     
             player_ = other_player();
             action_number += 1;
+            
+            // Case the game ends in a draw
             if (is_terminal_flag == 0) {
-                if (action_number == MAX_VALUE_ACTION){
+                if (action_number == MAX_SQUARES){
                     is_terminal_flag = 2;
                 }
             }
-            
         }
     }
       
     // case current player is Player 2
     else {
         // calculate opponent past actions 
-        int past_actions[MAX_SQUARES] = {0,0,0,0,0,0,0,0};
+        int past_actions[MAX_SQUARES] = {0,0,0,0,0,0,0,0, 0};
+        
         for (const auto& a : pieces_P1_){
                int idx = actions_to_idx(a);
                past_actions[idx] = 1;
            }
+        
         unsigned int current_action_idx = actions_to_idx(a);
         
         // case illegal move
@@ -124,15 +112,17 @@ namespace oz {
             pieces_P2_.push_back(a);
             pieces_P2_.push_back(action_t::NextRound);
             history_.push_back(action_t::NextRound);
+            
             is_terminal_flag = is_winning_move_vector(pieces_P2_);
             player_ = other_player();
             action_number += 1;
+            
+            // Case the game ends in a draw
             if (is_terminal_flag == 0) {
-                if (action_number == MAX_VALUE_ACTION){
+                if (action_number == MAX_SQUARES){
                 is_terminal_flag = 2;
                 }
             }
-            
         }
     }
   }
@@ -142,9 +132,12 @@ namespace oz {
 
   
   auto tic_tac_toes_t::utility(player_t player) const -> value_t {
+      
     assert (is_terminal());
 
     value_t u;
+      
+    // if terminal_flag is set to 2 it means that it's a draw
     if (is_terminal_flag == 2){
       u = 0;
     }
@@ -161,292 +154,290 @@ namespace oz {
         }
       
     return u;
-    
       
   }
 
   
       
+  // Function that calculate is the last action put a piece in the board that lead to a victory
   auto tic_tac_toes_t::is_winning_move_vector(action_vector_t moves) -> int{
     
-    
-    unsigned int end_of_the_game = 0;
-    int tot_moves[MAX_VALUE_ACTION] = {0,0,0,0,0,0,0,0,0};
-    
     unsigned int moves_size = moves.size();
+    unsigned int end_of_the_game = 0;
+    int tot_moves[MAX_SQUARES] = {0,0,0,0,0,0,0,0,0};
+    
     for (unsigned int i = 0; i < moves_size; ++i){
-            if(actions_to_idx(moves[i]) == -1){
+        
+            // the only action with index -1 is Next Round
+            // We iterate until we reach Next Round and then consider the action before that and we store it in tot_moves
+            // Tot_moves will have all the legal actions of the current player set to 1 
+        
+            if(actions_to_idx(moves[i]) == -1)
                 tot_moves[actions_to_idx(moves[i - 1])] = 1;
-            }
+            
     }
       
     
         
-        if (tot_moves[0] == 1){
-          /* case
-           x - -
-           x - -
-           x - -
-           */
-          if (tot_moves[1] == 1 and tot_moves[2] == 1){
+    if (tot_moves[0] == 1){
+        /* case
+        x - -
+        x - -
+        x - -
+        */
+        if (tot_moves[1] == 1 and tot_moves[2] == 1){
             end_of_the_game = 1;
-          }
-          /* case
-           - - -
-           - - -
-           x x x
-           */
-          else if (tot_moves[3] == 1 and tot_moves[6] == 1){
-            end_of_the_game = 1;
-          }
-          /* case
-           - - x
-           - x -
-           x - -
-           */
-          else if (tot_moves[4] == 1 and tot_moves[8] == 1){
-            end_of_the_game = 1;
-          }
         }
         /* case
-         - - -
-         x x x
-         - - -
-         */
-        if (tot_moves[1] == 1 and tot_moves[4] == 1 and tot_moves[7] == 1){
-          end_of_the_game = 1;
-        }
-        if (tot_moves[2] == 1) {
-          
-          /* case
-           x x x
-           - - -
-           - - -
-           */
-          if (tot_moves[5] == 1 and tot_moves[8] == 1){
+        - - -
+        - - -
+        x x x
+        */
+        else if (tot_moves[3] == 1 and tot_moves[6] == 1){
             end_of_the_game = 1;
-          }
-          /* case
-           x - -
-           - x -
-           - - x
-           */
-          else if (tot_moves[4] == 1 and tot_moves[6] == 1){
-            end_of_the_game = 1;
-          }
-          
         }
         /* case
-         - x -
-         - x -
-         - x -
-         */
+        - - x
+        - x -
+        x - -
+        */
+        else if (tot_moves[4] == 1 and tot_moves[8] == 1){
+            end_of_the_game = 1;
+        }
+    }
+    /* case
+    - - -
+    x x x
+    - - -
+    */
+    if (tot_moves[1] == 1 and tot_moves[4] == 1 and tot_moves[7] == 1){
+        end_of_the_game = 1;
+    }
+    if (tot_moves[2] == 1) {
+        /* case
+        x x x
+        - - -
+        - - -
+        */
+        if (tot_moves[5] == 1 and tot_moves[8] == 1){
+            end_of_the_game = 1;
+        }
         
-        if (tot_moves[3] == 1 and tot_moves[4] == 1 and tot_moves[5] == 1){
-          end_of_the_game = 1;
-          
-        }
         /* case
-         - - x
-         - - x
-         - - x
-         */
-        if (tot_moves[6] == 1 and tot_moves[7] == 1 and tot_moves[8] == 1){
-          end_of_the_game = 1;
-          
+        x - -
+        - x -
+        - - x
+        */
+        else if (tot_moves[4] == 1 and tot_moves[6] == 1){
+            end_of_the_game = 1;
         }
-        return end_of_the_game;
+    }
+    
+    /* case
+    - x -
+    - x -
+    - x -
+    */
+    if (tot_moves[3] == 1 and tot_moves[4] == 1 and tot_moves[5] == 1){
+        end_of_the_game = 1;
+    }
+         
+    /* case
+    - - x
+    - - x
+    - - x
+    */
+    if (tot_moves[6] == 1 and tot_moves[7] == 1 and tot_moves[8] == 1){
+          end_of_the_game = 1;
+    }
+          
+    
+    return end_of_the_game;
+    
+    }
+    
+    
+    
+    auto tic_tac_toes_t::infoset() const -> oz::infoset_t {
+        return make_infoset<infoset_t>(player_, pieces_P1_, pieces_P2_, history_, action_number, is_terminal_flag);
+    }
+  
+
+    auto tic_tac_toes_t::infoset(oz::infoset_t::allocator_t alloc) const -> oz::infoset_t {
+        return allocate_infoset<infoset_t, oz::infoset_t::allocator_t>
+            (alloc,
+             player_, pieces_P1_, pieces_P2_, history_,
+             action_number, is_terminal_flag);
+    }
+
+    // Is terminal will return 0 fi the game is not finished, 2 if the game finished with a draw, 1 otherwise
+    auto tic_tac_toes_t::is_terminal() const -> bool {
+        return is_terminal_flag;
+    }
+  
+
+    auto tic_tac_toes_t::infoset_t::actions() const -> actions_list_t {
+    
+        actions_list_t actions;
+        int past_actions[MAX_SQUARES] = {0,0,0,0,0,0,0,0,0};
+        unsigned int i;
+
+
+        // Player P1
+        if (player_idx(player) == 0){
+            
+          // if it's the frist action all the actions are available
+          if (pieces_P1.empty()){
+              for (i = 0; i < MAX_SQUARES; ++i)
+                  actions.push_back(make_action(i));
+           }
+           else {
+               for (const auto& a : pieces_P1){
+                   int idx = actions_to_idx(a);
+                   past_actions[idx] = 1;
+               }
+               for (i = 0; i < MAX_SQUARES; ++i){
+                   if(past_actions[i] == 0)
+                       actions.push_back(make_action(i));
+               }
+           }
+        }
+        // Player P2
+        else{
+          if (pieces_P2.empty()){
+              for (i = 0; i < MAX_SQUARES; ++i)
+                  actions.push_back(make_action(i));
+           }
+           else {
+               for (const auto& a : pieces_P2){
+                   int idx = actions_to_idx(a);
+                   past_actions[idx] = 1;
+               }
+               for (i = 0; i < MAX_SQUARES; ++i){
+                   if(past_actions[i] == 0)
+                       actions.push_back(make_action(i));
+                   }
+               }
+          }
+
+        return actions;
+    }
+
+
+
+    auto tic_tac_toes_t::chance_actions(action_prob_allocator_t alloc) const -> action_prob_map_t {
+        Expects(false);
+        return { };
+    }
+
+    auto tic_tac_toes_t::chance_actions() const -> action_prob_map_t {
+        return tic_tac_toes_t::chance_actions({ });
+    }
+
+  
+    static std::ostream& operator << (std::ostream& os, const tic_tac_toes_t::action_t &action) {
         
-      }
-  auto tic_tac_toes_t::infoset() const -> oz::infoset_t {
-    Expects(player() != CHANCE);
-    return make_infoset<infoset_t>(player_, pieces_P1_, pieces_P2_, history_, action_number, is_terminal_flag);
+        using action_t = tic_tac_toes_t::action_t;
 
-  }
-  
+        if (action == action_t::fill_1) {
+          os << '1';
+        }
+        else if (action == action_t::fill_2) {
+          os << '2';
+        }
+        else if (action == action_t::fill_3) {
+          os << '3';
+        }
+        else if (action == action_t::fill_4) {
+          os << '4';
+        }
+        else if (action == action_t::fill_5) {
+          os << '5';
+        }
+        else if (action == action_t::fill_6) {
+          os << '6';
+        }
+        else if (action == action_t::fill_7) {
+          os << '7';
+        }
+        else if (action == action_t::fill_8) {
+          os << '8';
+        }
+        else if (action == action_t::fill_9) {
+          os << '9';
+        }
+        else if (action == action_t::NextRound) {
+            os << '/';
+        }
+        else {
+          os << '?';
+          getchar();
+        }
 
-  auto tic_tac_toes_t::infoset(oz::infoset_t::allocator_t alloc) const
-  -> oz::infoset_t
-  {
-    Expects(player() != CHANCE);
-    
-    return allocate_infoset<infoset_t, oz::infoset_t::allocator_t>
-        (alloc,
-         player_, pieces_P1_, pieces_P2_, history_,
-         action_number, is_terminal_flag);
-  }
-
-  auto tic_tac_toes_t::is_terminal() const -> bool {
-    return is_terminal_flag;
-    
-  }
-
-
-  
-
-  auto tic_tac_toes_t::infoset_t::actions() const -> actions_list_t {
-    
-    actions_list_t actions;
-    int past_actions[MAX_SQUARES] = {0,0,0,0,0,0,0,0};
-     
-    
-    // Player P1
-    if (player_idx(player) == 0){
-      if (history.empty()){
-          for (unsigned int i = 0; i < MAX_SQUARES; ++i)
-              actions.push_back(make_action(i));
-       }
-       else {
-           for (const auto& a : pieces_P1){
-               int idx = actions_to_idx(a);
-               past_actions[idx] = 1;
-           }
-           for (unsigned int i = 0; i < MAX_SQUARES; ++i){
-               if(past_actions[i] == 0)
-                   actions.push_back(make_action(i));
-           }
-           
-           
-       }
-    }
-    // Player P2
-    else{
-      if (history.empty()){
-          for (unsigned int i = 0; i < MAX_SQUARES; ++i)
-              actions.push_back(make_action(i));
-       }
-       else {
-           for (const auto& a : pieces_P2){
-               int idx = actions_to_idx(a);
-               past_actions[idx] = 1;
-           }
-           for (unsigned int i = 0; i < MAX_SQUARES; ++i){
-               if(past_actions[i] == 0)
-                   actions.push_back(make_action(i));
-           }
-       }
-      }
-    
-    return actions;
-  }
-
-
-
-  auto tic_tac_toes_t::chance_actions(action_prob_allocator_t alloc) const -> action_prob_map_t {
-    Expects(player() == CHANCE);
-    
-    return { };
-
-  }
-
-  auto tic_tac_toes_t::chance_actions() const -> action_prob_map_t {
-    return tic_tac_toes_t::chance_actions({ });
-  }
-
-  
-  static std::ostream& operator << (std::ostream& os,
-                                   const tic_tac_toes_t::action_t &action)
-  {
-    using action_t = tic_tac_toes_t::action_t;
-    
-    if (action == action_t::fill_1) {
-      os << '1';
-    }
-    else if (action == action_t::fill_2) {
-      os << '2';
-    }
-    else if (action == action_t::fill_3) {
-      os << '3';
-    }
-    else if (action == action_t::fill_4) {
-      os << '4';
-    }
-    else if (action == action_t::fill_5) {
-      os << '5';
-    }
-    else if (action == action_t::fill_6) {
-      os << '6';
-    }
-    else if (action == action_t::fill_7) {
-      os << '7';
-    }
-    else if (action == action_t::fill_8) {
-      os << '8';
-    }
-    else if (action == action_t::fill_9) {
-      os << '9';
-    }
-    else if (action == action_t::NextRound) {
-        os << '/';
-    }
-    else {
-      os << '?';
-      getchar();
-    }
-    
-    return os;
-  }
-
-  auto tic_tac_toes_t::infoset_t::str() const -> std::string {
-    stringstream ss;
-
-    if (!history.empty()) {
-        ss << "/";
+        return os;
     }
 
-    for (const auto& a : history) {
-        ss << a;
-    }
+    auto tic_tac_toes_t::infoset_t::str() const -> std::string {
+        stringstream ss;
 
-    return ss.str();
-}
+        if (!history.empty()) {
+            ss << "/";
+        }
+
+        for (const auto& a : history) {
+            ss << a;
+        }
+
+        return ss.str();
+    }
       
       
-  auto tic_tac_toes_t::str() const -> std::string {
-      stringstream ss;
+    auto tic_tac_toes_t::str() const -> std::string {
+        stringstream ss;
 
-      if (!history().empty()) {
-        ss << "/";
-      }
+        if (!history().empty()) {
+            ss << "/";
+        }
 
-      for (const auto& a : history()) {
-        ss << a;
-      }
+        for (const auto& a : history()) {
+            ss << a;
+        }
 
-      return ss.str();
-}
-
-
-  bool tic_tac_toes_t::infoset_t::is_equal(const infoset_t::concept_t &that) const {
-    if (typeid(*this) == typeid(that)) {
-      auto that_ = static_cast<const tic_tac_toes_t::infoset_t &>(that);
-      
-      return player == that_.player &&
-             pieces_P1 == that_.pieces_P1 &&
-             pieces_P2 == that_.pieces_P2 &&
-             history == that_.history;
+        return ss.str();
     }
-    else {
-      
-      return false;
-    }
-    
-    
-  }
 
-  size_t tic_tac_toes_t::infoset_t::hash() const {
-    size_t seed = 0;
-    hash_combine(seed, player);
-    for (const auto &a : pieces_P1) {
-      hash_combine(seed, a);
+
+    bool tic_tac_toes_t::infoset_t::is_equal(const infoset_t::concept_t &that) const {
+        if (typeid(*this) == typeid(that)) {
+          auto that_ = static_cast<const tic_tac_toes_t::infoset_t &>(that);
+
+          return player == that_.player &&
+                 pieces_P1 == that_.pieces_P1 &&
+                 pieces_P2 == that_.pieces_P2 &&
+                 history == that_.history;
+        }
+        else {
+            return false;
+        }
     }
-    for (const auto &a : pieces_P2) {
-      hash_combine(seed, a);
+
+    size_t tic_tac_toes_t::infoset_t::hash() const {
+        
+        size_t seed = 0;
+        hash_combine(seed, player);
+        
+        for (const auto &a : pieces_P1) {
+          hash_combine(seed, a);
+        }
+        
+        for (const auto &a : pieces_P2) {
+          hash_combine(seed, a);
+        }
+        
+        for (const auto &a : history) { hash_combine(seed, a); }
+
+        return seed;
     }
-    for (const auto &a : history) { hash_combine(seed, a); }
-    
-    return seed;
-  }
   
 
 } // namespace oz
--- oz/csrc/games/tic_tac_toes.h
+++ oz/csrc/games/tic_tac_toes.h
@@ -5,10 +5,7 @@
 #include "game.h"
 
 #include <cassert>
-#include <array>
-#include <set>
 #include <vector>
-#include <bitset>
 
 #include <boost/container/static_vector.hpp>
 
@@ -16,11 +13,7 @@ namespace oz {
 
   using std::move;
   using std::string;
-  using std::array;
-  using std::set;
   using std::vector;
-  using std::vector;
-  using std::bitset;
   
   using boost::container::static_vector;
 
@@ -32,7 +25,6 @@ namespace oz {
       enum class action_t {
 
       /* actions for P1 and P2 :
-       P1 is the one playing X, P2 plays 0
        The actions are fill_1 to fill_9 numbered as follow:
        
        3 6 9
@@ -51,30 +43,26 @@ namespace oz {
       fill_8,
       fill_9 = 8,
           
-      NextRound = 100
+      NextRound = -1
 
-      // There is no chance player
     };
 
-    
-    static constexpr int MAX_HISTORY = 26;
     // max history is 1/12/23/34/45/56/67/78/89/
+    static constexpr int MAX_HISTORY = 26;
       
     using action_vector_t = static_vector<action_t, MAX_HISTORY>;
    
-
     
     struct infoset_t : public oz::infoset_t::concept_t {
       const player_t player;
-      const action_vector_t pieces_P1;
-      const action_vector_t pieces_P2;
-      const action_vector_t history;
-      const int action_number;
-      
+      const action_vector_t pieces_P1;   // All the actions made by P1
+      const action_vector_t pieces_P2;   // All the actions made by P1
+      const action_vector_t history;     // All the actions made by both P1 and P2
+      const int action_number;           // Total number of actions in the game
       const int is_terminal_flag;
       
 
-      infoset_t(player_t player,action_vector_t pieces_P1, action_vector_t pieces_P2, action_vector_t history, int action_number, int is_terminal_flag):
+      infoset_t(player_t player, action_vector_t pieces_P1, action_vector_t pieces_P2, action_vector_t history, int action_number, int is_terminal_flag):
           player(player),pieces_P1(move(pieces_P1)), pieces_P2(move(pieces_P2)), history(move(history)), action_number(action_number),is_terminal_flag(is_terminal_flag){ }
 
       actions_list_t actions() const override;
@@ -88,35 +76,31 @@ namespace oz {
     void act(oz::action_t a) override { act_(a.cast<action_t>()); }
     oz::infoset_t infoset() const override;
     player_t player() const override { return player_; }
-       bool is_terminal() const override;
+    bool is_terminal() const override;
     value_t utility(player_t player) const override;
     oz::infoset_t infoset(oz::infoset_t::allocator_t alloc) const override;
     action_prob_map_t chance_actions() const override;
     std::string str() const override;
-      
     
 
     std::unique_ptr<game_t> clone() const override {
       return std::make_unique<tic_tac_toes_t>(*this);
     }
 
-
     action_prob_map_t chance_actions(action_prob_allocator_t alloc) const override;
     
-    static constexpr int MAX_VALUE_ACTION = 9;
    
 
   private:
     
-    // First player is P1
     player_t player_ = P1;
     
-    int round_ = 0;
     int action_number = 0;
+    int turn_ = 0;
     int is_terminal_flag = 0;
+    
     action_vector_t pieces_P1_;
     action_vector_t pieces_P2_;
-    int turn_ = 0;
     action_vector_t history_;
     
     
