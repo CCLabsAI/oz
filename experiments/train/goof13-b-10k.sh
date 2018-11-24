64ca0367727bc580e66da7bcb506ca843419efc4
Andy Kitchen <andy.kitchen@cclabs.ai>
2018-11-24 19:11:27 +1100

    train: overhaul of experiments

--- experiments/train/goof13-b-10k.sh
+++ experiments/train/goof13-b-10k.sh
@@ -1,4 +1,4 @@
-EXPERIMENT=holdem-demo-10k
+EXPERIMENT=goof13-b-10k
 
 SRC_DIR=/mnt/src/oz
 PYTHON=/mnt/miniconda3/bin/python
@@ -7,19 +7,20 @@ LOG_PATH=/mnt/exp/${EXPERIMENT}.txt
 
 OMP_NUM_THREADS=1 \
 PYTHONPATH=$SRC_DIR \
-nohup nice $PYTHON $SRC_DIR/script/train.py \
-	--game=holdem \
+nohup $PYTHON $SRC_DIR/script/train.py \
+	--game=goofspiel \
+	--goofcards=13 \
+	--dist --workers=32 \
 	--checkpoint_dir=$CHECKPOINT_DIR \
-	--dist --workers=64 \
 	--checkpoint_interval=1 \
 	--train_iter=2500 \
-	--train_game_ply=16 \
-	--train_batch_size=$[16*32] \
+	--train_game_ply=13 \
+	--train_batch_size=$[13*32] \
 	--train_steps=512 \
 	--nn_arch=deep \
 	--hidden_sizes="256:256:128" \
 	--search_batch_size=20 \
-	--reservoir_size=$[2**22] \
+	--reservoir_size=$[1000*13*32] \
 	--reservoir_beta_ratio=2.0 \
 	--simulation_iter=10000 \
 	--progress \
