3570a297683d48813da8dbda24d06b072b28ab13
Andy Kitchen <andy.kitchen@cclabs.ai>
2018-11-27 14:36:11 +1100

    demo: greatly improved poker backend

--- script/server.py
+++ script/server.py
@@ -3,6 +3,7 @@
 import os
 import re
 import argparse
+import numpy as np
 
 from bottle import Bottle, request
 
@@ -11,15 +12,27 @@ import torch.nn.functional as F
 import oz
 import oz.nn
 
+import log2vector
+import train_utility
+import train_actions
 
 TEST_POSIBLE_HANDS=[[['AC', '2H', '3S', '4D', '5C']], [['AC', 'AH'],['5C', '5D']], [['AC', 'AD'],['5C', '5D', '5S']], [['5C', '5H', '5S']], [['JH', 'JC']]]
 
 app = Bottle()
 
+
+def assert_loaded(model, load_state):
+    new_state = model.state_dict()
+    assert new_state.keys() == load_state.keys()
+    for k in new_state:
+        assert torch.equal(new_state[k].cpu(), load_state[k].cpu()), "{} not equal".format(k)
+
+
 encoder = oz.make_holdem_encoder()
 
 ob = torch.load(os.path.expanduser('~/data/misc/checkpoint-000005.pth'))
 args = argparse.Namespace(**ob['args'])
+state_dict = ob['model_state']
 
 print(args)
 
@@ -27,13 +40,38 @@ model = oz.nn.model_with_args(args,
             input_size=encoder.encoding_size(),
             output_size=encoder.max_actions())
 
-print(model)
+model.load_state_dict(state_dict)
+assert_loaded(model, state_dict)
+model.eval()
 
-model.load_state_dict(ob['model_state'])
+utility_model = train_utility.build_model()
+
+path = os.path.expanduser("~/src/poker-predict/models/poker_utility_model_demo1.pth")
+state_dict = torch.load(path, map_location='cpu')
+
+utility_model.load_state_dict(state_dict)
+assert_loaded(utility_model, state_dict)
+utility_model.eval()
+
+print(utility_model)
+
+
+action_model = train_actions.build_model()
+
+path = os.path.expanduser("~/src/poker-predict/models/poker_action_model_demo1.pth")
+state_dict = torch.load(path, map_location='cpu')
+
+action_model.load_state_dict(state_dict)
+assert_loaded(action_model, state_dict)
+action_model.eval()
+
+print(action_model)
+
 
 rng = oz.Random(1)
 
-ACTION_NAMES = [None, 'raise', 'call', 'fold']
+OZ_ACTION_NAMES = [None, 'raise', 'call', 'fold']
+ACTION_NAMES0 = ['raise', 'call', 'fold']
 
 def lower_suit(card):
     r, s = card
@@ -45,7 +83,8 @@ def index():
     print(request.json)
 
     json = request.json
-    seq_no_blind = re.sub("^/bb", "/", json['seq'])
+    seq_no_blind = re.sub("^/rr", "/", json['seq'])
+    seq_no_blind = re.sub("^/", "", seq_no_blind)
     ai_hand = "".join([lower_suit(c) for c in json['cards']['AI']])
     player_hand = "".join([lower_suit(c) for c in json['cards']['player']])
     hands = ai_hand + '|' + player_hand
@@ -56,36 +95,73 @@ def index():
     if len(seq_no_blind) > 0:
         hist_str += ":" + seq_no_blind
 
-
     print(hist_str)
 
-    h = oz.make_holdem_history()
-    g = h.game
-    g.read_history_str(hist_str)
-    tensor = torch.zeros(encoder.encoding_size())
-    infoset = h.infoset()
-    encoder.encode(infoset, tensor)
-    logits = model.forward(tensor)
-    probs = F.softmax(logits, dim=0)
-    ap = encoder.decode_and_sample(infoset, probs, rng)
-    action_name = ACTION_NAMES[ap.a.index]
+    # h = oz.make_holdem_history()
+    # g = h.game
+    # g.read_history_str(hist_str)
+    # tensor = torch.zeros(encoder.encoding_size())
+    # infoset = h.infoset()
+    # encoder.encode(infoset, tensor)
+    # logits = model.forward(tensor)
+    # probs = F.softmax(logits, dim=0)
+    # ap = encoder.decode_and_sample(infoset, probs, rng)
+    # action_name = OZ_ACTION_NAMES[ap.a.index]
 
-    return {
+    def player_for_log(log):
+        fp = log2vector.FIRST_PLAYER[len(log.history)-1]
+        p = (fp + len(log.history[-1])) % 2
+        return p
+
+    log = log2vector.parse_history(bytes(hist_str, 'ASCII'))
+    p = player_for_log(log)
+
+    state = log2vector.State(
+                    history=log.history,
+                    hole=log.hole[p],
+                    board=log.board,
+                    utility=None,
+                    current_player=p,
+                    chosen_action=None)
+
+    print(state)
+
+    state_vector = log2vector.state_to_vector(state)
+    state_tensor = torch.from_numpy(state_vector.astype(np.float32))
+
+    a_logits = action_model.forward(state_tensor)
+    dist = torch.distributions.Categorical(logits=a_logits)
+    a = dist.sample().item()
+    action_name = ACTION_NAMES0[a]
+
+    v = utility_model.forward(state_tensor).item()
+    v *= train_utility.UTILITY_SCALE
+
+    ret_json = {
         'action': action_name,
         'bluff': 50,
         'possibleHands': TEST_POSIBLE_HANDS,
-        'moneyProspection': 100,
+        'moneyProspection': v,
         'winner': None
     }
 
-@app.route('/api/winner', method='POSTq')
-def index():
-    print(request.json)
-    return {
-        'winner': 'AI',
-        'reason': 'you smell',
-        'AIHand': 'super duper pooper',
-        'playerHand': 'smelly nutz'
-    }
+    print(ret_json)
+    return ret_json
 
-app.run(host='localhost', port=8080, debug=True)
+
+def live_debugger(callback):
+    import sys, pdb, traceback
+    def wrapper(*args,**kwargs):
+        try:
+            body = callback(*args,**kwargs)
+            return body
+        except:
+            type, value, tb = sys.exc_info()
+            if type is not KeyboardInterrupt:
+                pdb.post_mortem(tb)
+    return wrapper
+
+
+if __name__ == '__main__':
+    # app.install(live_debugger)
+    app.run(host='localhost', port=8080, debug=True)
